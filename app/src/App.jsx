a6beb5cf83d1de9da0179cce9b84e5f8d020f73e
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2025-02-06 21:15:44 +0600

    Show better card flip animation

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,197 +2,200 @@ import { useEffect, useState } from "react";
 import { helpers } from "./helpers";
 import "./style/App.css";
 function App() {
-	const [ui, updateUi] = useState("home");
-	const [mode, updateMode] = useState("");
-	const [cards, setCards] = useState([]);
-	const [modifiedCards, setModifiedCards] = useState([]);
-	const [score, updateScore] = useState(0);
-	const [highestScore, setHighestScore] = useState({
-		easy: 0,
-		normal: 0,
-		hard: 0,
-	});
-	const [result, setResult] = useState("");
-	const modes = ["easy", "normal", "hard"];
+  const [ui, updateUi] = useState("home");
+  const [mode, updateMode] = useState("");
+  const [cards, setCards] = useState([]);
+  const [modifiedCards, setModifiedCards] = useState([]);
+  const [score, updateScore] = useState(0);
+  const [highestScore, setHighestScore] = useState({
+    easy: 0,
+    normal: 0,
+    hard: 0,
+  });
+  const [result, setResult] = useState("");
+  const modes = ["easy", "normal", "hard"];
+  const [displayedCards, setDisplayedCards] = useState([]);
 
-	function displayCards(_cards) {
-		const displayedCards = helpers.selectCardsForDisplay(_cards, mode);
-		if (displayedCards.length === 0) {
-			setResult("win");
-			handleGameOver();
-		}
-		return (
-			<>
-				<div className="cards-container">
-					{displayedCards.map((eachCard) => {
-						return (
-							<div
-								onClick={onCardClicked}
-								data-name={eachCard.name}
-								// data-flipped="true"
-								className="card"
-								key={eachCard.name}
-							>
-								<div className="inner">
-									<div className="front">
-										<img
-											className="card-img"
-											src={eachCard.img}
-											alt={eachCard.name}
-										/>
-										<h2 className="card-title">{eachCard.name}</h2>
-									</div>
-									<h2 className="back"></h2>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-				{/* {toggleFlippedData("remove")} */}
-			</>
-		);
-	}
-	function handleGameOver() {
-		setModifiedCards([]);
-		updateUi("game-over");
-		if (score >= highestScore[mode]) {
-			setHighestScore({
-				...highestScore,
-				[mode]: score,
-			});
-		}
-	}
-	function toggleFlippedData(addOrRemove) {
-		const innerCards = document.querySelectorAll(".inner");
-		innerCards.forEach((item) => {
-			if (addOrRemove === "add") item.dataset.flipped = "true";
-			else item.dataset.flipped = "";
-		});
-	}
-	function onCardClicked(e) {
-		toggleFlippedData("add");
-		const title = e.currentTarget.querySelector(".card-title").textContent;
-		setTimeout(() => {
-			const tempCards = modifiedCards.map((card) => {
-				if (card.name === title) {
-					if (card.selected === true) handleGameOver();
-					else {
-						updateScore(score + 1);
-						if (score >= highestScore[mode]) {
-							setHighestScore({
-								...highestScore,
-								[mode]: score + 1,
-							});
-						}
-						return { ...card, selected: true };
-					}
-				} else return card;
-			});
-			setModifiedCards([...helpers.shuffleArray(tempCards)]);
-			toggleFlippedData("remove");
-		}, 600);
-	}
-	function displayModes(_modes) {
-		return (
-			<div className="modes">
-				{_modes.map((_mode) => {
-					return (
-						<button
-							onClick={() => {
-								updateMode(_mode);
-								updateUi("start-game");
-							}}
-							className={_mode}
-							key={_mode}
-						>
-							{_mode}
-						</button>
-					);
-				})}
-			</div>
-		);
-	}
-	//fetching data from api
-	useEffect(() => {
-		fetch("https://api.jikan.moe/v4/top/characters", { mode: "cors" })
-			.then((response) => {
-				if (!response.ok) throw new Error("Could not fetch data");
-				return response.json();
-			})
-			.then((response) => {
-				setCards(
-					response.data.map((item) => {
-						return {
-							name: item.name,
-							img: item.images.jpg.image_url,
-							selected: false,
-						};
-					})
-				);
-			})
-			.catch((error) => alert(error));
-	}, []);
-	//starting main game
-	useEffect(() => {
-		function startGame() {
-			updateUi("playing");
-			if (mode == "easy") {
-				setModifiedCards(
-					helpers.shuffleArray(cards).filter((element, index) => index < 10)
-				);
-			}
-			if (mode == "normal")
-				setModifiedCards(
-					helpers.shuffleArray(cards).filter((element, index) => index < 15)
-				);
-			if (mode == "hard")
-				setModifiedCards(
-					helpers.shuffleArray(cards).filter((element, index) => index < 25)
-				);
-		}
-		if (ui === "start-game" && cards.length > 0 && mode) {
-			startGame();
-		}
-	}, [ui, cards, mode]);
-	function reset() {
-		updateUi("home");
-		updateMode("");
-		setModifiedCards([]);
-		updateScore(0);
-		setResult("");
-	}
-	return (
-		<>
-			{(ui === "playing" || ui === "game-over") && (
-				<div className="scoreboard">
-					<div className="current-score">CURRENT SCORE = {score}</div>
-					<div className="highest-score">
-						HIGHEST SCORE ={highestScore[mode]}
-					</div>
-				</div>
-			)}
-			{ui == "home" && (
-				<button className="btn-play" onClick={() => updateUi("modes")}>
-					Play
-				</button>
-			)}
-			{ui === "modes" && displayModes(modes)}
-			{ui === "start-game" && cards.length < 1 && (
-				<h1 className="loading">loading...</h1>
-			)}
-			{ui === "playing" && displayCards(modifiedCards)}
-			{ui === "game-over" && (
-				<div className="game-over">
-					<h1 className="game-over-title">
-						Game Over. {result === "win" ? <>You win</> : null}
-					</h1>
-					<button className="btn-play-again" onClick={reset}>
-						Play again{" "}
-					</button>
-				</div>
-			)}
-		</>
-	);
+  function selectCardsToDisplay() {
+    const a = helpers.selectCardsForDisplay(modifiedCards, mode);
+    if (a.length === 0) {
+      setResult("win");
+      handleGameOver();
+    }
+    setDisplayedCards([...a]);
+  }
+  function Cards() {
+    return (
+      <>
+        <div className="cards-container">
+          {displayedCards.map((eachCard) => {
+            return (
+              <div
+                onClick={onCardClicked}
+                data-name={eachCard.name}
+                // data-flipped="true"
+                className="card"
+                key={eachCard.name}
+              >
+                <div className="inner">
+                  <div className="front">
+                    <img
+                      className="card-img"
+                      src={eachCard.img}
+                      alt={eachCard.name}
+                    />
+                    <h2 className="card-title">{eachCard.name}</h2>
+                  </div>
+                  <h2 className="back"></h2>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {/* {toggleFlippedData("remove")} */}
+      </>
+    );
+  }
+  function handleGameOver() {
+    // setModifiedCards([]);
+    updateUi("game-over");
+    if (score >= highestScore[mode]) {
+      setHighestScore({
+        ...highestScore,
+        [mode]: score,
+      });
+    }
+  }
+  function onCardClicked(e) {
+    const title = e.currentTarget.querySelector(".card-title").textContent;
+    setTimeout(() => {
+      const tempCards = modifiedCards.map((card) => {
+        if (card.name === title) {
+          if (card.selected === true) {
+            handleGameOver();
+            return card;
+          } else {
+            updateScore(score + 1);
+            if (score >= highestScore[mode]) {
+              setHighestScore({
+                ...highestScore,
+                [mode]: score + 1,
+              });
+            }
+            return { ...card, selected: true };
+          }
+        } else return card;
+      });
+      setModifiedCards([...helpers.shuffleArray(tempCards)]);
+      // toggleFlippedData("remove");
+    }, 300);
+  }
+  function displayModes(_modes) {
+    return (
+      <div className="modes">
+        {_modes.map((_mode) => {
+          return (
+            <button
+              onClick={() => {
+                updateMode(_mode);
+                updateUi("start-game");
+              }}
+              className={_mode}
+              key={_mode}
+            >
+              {_mode}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+  //fetching data from api
+  useEffect(() => {
+    fetch("https://api.jikan.moe/v4/top/characters", { mode: "cors" })
+      .then((response) => {
+        if (!response.ok) throw new Error("Could not fetch data");
+        return response.json();
+      })
+      .then((response) => {
+        setCards(
+          response.data.map((item) => {
+            return {
+              name: item.name,
+              img: item.images.jpg.image_url,
+              selected: false,
+            };
+          })
+        );
+      })
+      .catch((error) => alert(error));
+  }, []);
+  //starting main game
+  useEffect(() => {
+    function startGame() {
+      updateUi("playing");
+      if (mode == "easy") {
+        setModifiedCards(
+          helpers.shuffleArray(cards).filter((element, index) => index < 10)
+        );
+      }
+      if (mode == "normal")
+        setModifiedCards(
+          helpers.shuffleArray(cards).filter((element, index) => index < 15)
+        );
+      if (mode == "hard")
+        setModifiedCards(
+          helpers.shuffleArray(cards).filter((element, index) => index < 25)
+        );
+    }
+    if (ui === "start-game" && cards.length > 0 && mode) {
+      startGame();
+    }
+    if (modifiedCards.length > 0) selectCardsToDisplay();
+  }, [ui, cards, mode, modifiedCards]);
+  function reset() {
+    updateUi("home");
+    updateMode("");
+    setModifiedCards([]);
+    updateScore(0);
+    setResult("");
+  }
+  return (
+    <>
+      {(ui === "playing" || ui === "game-over") && (
+        <>
+          {" "}
+          <div className="scoreboard">
+            <div className="current-score">CURRENT SCORE = {score}</div>
+            <div className="highest-score">
+              HIGHEST SCORE ={highestScore[mode]}
+            </div>
+          </div>
+          <div className="instruction">If you select one card twice, you lose.</div>
+        </>
+      )}
+      {ui == "home" && (
+        <button className="btn-play" onClick={() => updateUi("modes")}>
+          Play
+        </button>
+      )}
+      {ui === "modes" && displayModes(modes)}
+      {ui === "start-game" && cards.length < 1 && (
+        <h1 className="loading">loading...</h1>
+      )}
+      {ui === "playing" && <Cards />}
+      {ui === "game-over" && (
+        <div className="game-over">
+          <h1 className="game-over-title">
+            Game Over. {result === "win" ? <>You win</> : null}
+          </h1>
+          <button className="btn-play-again" onClick={reset}>
+            Play again{" "}
+          </button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
--- app/src/helpers.js
+++ app/src/helpers.js
@@ -1,52 +1,55 @@
 const helpers = (() => {
-	function shuffleArray(array) {
-		for (let i = array.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[array[i], array[j]] = [array[j], array[i]]; // Swap elements
-		}
-		return array;
-	}
-	function getRandomInt(max) {
-		return Math.floor(Math.random() * max);
-	}
-	function areAllNumbersUnique(numbers) {
-		const uniqueNumbers = new Set(numbers);
-		return uniqueNumbers.size === numbers.length;
-	}
-	function randomArray(length, max) {
-		let arr = [];
-		for (let i = 0; i < length; i++) {
-			arr.push(getRandomInt(max));
-		}
-		if (areAllNumbersUnique(arr)) return arr;
-		else return randomArray(length, max);
-	}
-	function checkAllSelected(arrayOfCards) {
-		let allAreSelected = true;
-		for (let i = 0; i < arrayOfCards.length; i++) {
-			if (arrayOfCards[i].selected === false) {
-				allAreSelected = false;
-				break;
-			}
-		}
-		return allAreSelected;
-	}
-	function prepareCards(cards, length, max) {
-		const arrayOfCards = [];
-		const arrayOfIndexes = randomArray(length, max);
-		for (let i = 0; i < arrayOfIndexes.length; i++) {
-			arrayOfCards.push(cards[arrayOfIndexes[i]]);
-		}
-		if (checkAllSelected(arrayOfCards)) return prepareCards(cards, length, max);
-		return arrayOfCards;
-	}
-	//main card selector function
-	function selectCardsForDisplay(cards, mode) {
-		const limit =
-			mode === "easy" ? 4 : mode === "normal" ? 7 : mode === "hard" ? 10 : null;
-		if (checkAllSelected(cards)) return [];
-		else return prepareCards(cards, limit, cards.length);
-	}
-	return { shuffleArray, selectCardsForDisplay };
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function areAllNumbersUnique(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    return uniqueNumbers.size === numbers.length;
+  }
+  function randomArray(length, max) {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(getRandomInt(max));
+    }
+    if (areAllNumbersUnique(arr)) return arr;
+    else return randomArray(length, max);
+  }
+  function checkAllSelected(arrayOfCards) {
+    let allAreSelected = true;
+    console.log(arrayOfCards)
+    for (let i = 0; i < arrayOfCards.length; i++) {
+      
+      if (arrayOfCards[i].selected === false) {
+        allAreSelected = false;
+        break;
+      }
+    }
+    return allAreSelected;
+  }
+  function prepareCards(cards, limit, totalCards) {
+    const arrayOfCards = [];
+    const arrayOfIndexes = randomArray(limit, totalCards);
+    for (let i = 0; i < arrayOfIndexes.length; i++) {
+      arrayOfCards.push(cards[arrayOfIndexes[i]]);
+    }
+    if (checkAllSelected(arrayOfCards))
+      return prepareCards(cards, limit, totalCards);
+    return arrayOfCards;
+  }
+  //main card selector function
+  function selectCardsForDisplay(cards, mode) {
+    const limit =
+      mode === "easy" ? 4 : mode === "normal" ? 7 : mode === "hard" ? 10 : null;
+    if (checkAllSelected(cards)) return [];
+    else return prepareCards(cards, limit, cards.length);
+  }
+  return { shuffleArray, selectCardsForDisplay };
 })();
 export { helpers };
